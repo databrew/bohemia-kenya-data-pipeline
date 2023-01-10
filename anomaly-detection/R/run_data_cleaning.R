224bca2ce1db2905aff3b42b71c3b6840c015b93
Aryton Tediarjo <35942857+arytontediarjo@users.noreply.github.com>
2023-01-09 20:26:32 -0800

    update anomaly detection with cloudbrewr

--- anomaly-detection/R/run_data_cleaning.R
+++ anomaly-detection/R/run_data_cleaning.R
@@ -1,5 +1,6 @@
-#' @description: This script is purposed to clean survey forms and save to s3
-#' @author: atediarjo@gmail.com
+# This script is purposed to clean survey forms based on
+# user input and save the clean forms to S3
+# Author: atediarjo@gmail.com
 library(paws)
 library(dplyr)
 library(magrittr)
@@ -8,97 +9,61 @@ library(tidyr)
 library(data.table)
 library(glue)
 library(googlesheets4)
-source('R/utils.R')
+library(cloudbrewr)
 source('R/data_cleaning_function.R')
 
-svc <- paws::s3()
-S3_BUCKET_NAME <- glue::glue(Sys.getenv('BUCKET_PREFIX'), 'databrew.org')
 
+# set your pipeline stage here to define prod/dev environment
+# to test it locally do Sys.setenv(PIPELINE_STAGE = 'develop')
+cloudbrewr::aws_login(pipeline_stage = Sys.getenv('PIPELINE_STAGE'))
+
+# variables
+S3_BUCKET_NAME <- 'databrew.org'
 INPUT_KEY <- list(
   household =  'kwale/raw-form/reconbhousehold/reconbhousehold.csv',
   registration = "kwale/raw-form/reconaregistration/reconaregistration.csv",
   resolution = "kwale/anomalies/anomalies-resolution/anomalies-resolution.csv"
 )
 
+# output
 OUTPUT_KEY <- list(
   household = 'kwale/clean-form/reconbhousehold/reconbhousehold.csv',
   registration = "kwale/clean-form/reconaregistration/reconaregistration.csv"
 )
 
 
-# read local resolution file
-get_resolution <- function() {
-  filename <- tempfile()
-  bucket_name <- S3_BUCKET_NAME
-  get_s3_data(
-    s3obj = svc,
-    bucket= S3_BUCKET_NAME,
-    object_key = INPUT_KEY$resolution, # change this to clean data
-    filename = filename) %>%
-    fread(.) %>%
-    as_tibble() %>%
-    as_tibble(.name_repair = 'unique')
-}
-
-
-get_registration_data <- function(){
-  # Kwale Registration Forms
-  filename <- tempfile()
-  bucket_name <- S3_BUCKET_NAME
-  get_s3_data(
-    s3obj = svc,
-    bucket= S3_BUCKET_NAME,
-    object_key = INPUT_KEY$registration, # change this to clean data
-    filename = filename) %>%
-    fread(.) %>%
-    as_tibble() %>%
-    as_tibble(.name_repair = 'unique')
-}
-
-get_household_data <- function(){
-  # Kwale Household Forms
-  filename <- tempfile()
-  get_s3_data(
-    s3obj = svc,
-    bucket= S3_BUCKET_NAME,
-    object_key = INPUT_KEY$household, # change this to clean data
-    filename = filename) %>%
-    fread(.) %>%
-    as_tibble() %>%
-    as_tibble(.name_repair = 'unique')
-}
-
 # get resolution data
-resolution_data <- get_resolution()
-
+resolution_data <- cloudbrewr::aws_s3_get_table(
+  bucket = S3_BUCKET_NAME,
+  key = INPUT_KEY$resolution
+)
 
 # get registration forms and clean dataset
 filename <- tempfile(fileext = ".csv")
-registration <- get_registration_data() %>%
-  clean_registration_data(., resolution_data)
-
-# save registration file
-registration %>%
+registration <- cloudbrewr::aws_s3_get_table(
+  bucket = S3_BUCKET_NAME,
+  key = INPUT_KEY$registration) %>%
+  clean_registration_data(
+    resolution_file = resolution_data)  %>%
   fwrite(filename, row.names = FALSE)
-save_to_s3_bucket(
-  s3obj = svc,
-  file_path = filename,
-  bucket_name = S3_BUCKET_NAME,
-  object_key = OUTPUT_KEY$registration)
+# save registration data to s3 bucket
+cloudbrewr::aws_s3_store(
+  filename,
+  bucket = S3_BUCKET_NAME,
+  key = OUTPUT_KEY$registration)
 
 
-# get household forms and clean dataset
+
+# get household and clean household dataset
 filename <- tempfile(fileext = ".csv")
-
-# clean household
-household <- get_household_data() %>%
-  clean_household_data(., resolution_data)
-
-# save to s3 bucket
-household %>% fwrite(filename, row.names = FALSE)
-save_to_s3_bucket(
-  s3obj = svc,
-  file_path = filename,
-  bucket_name = S3_BUCKET_NAME,
-  object_key = OUTPUT_KEY$household)
-
+household <- cloudbrewr::aws_s3_get_table(
+  bucket = S3_BUCKET_NAME,
+  key = INPUT_KEY$household) %>%
+  clean_household_data(
+    resolution_file = resolution_data)  %>%
+  fwrite(filename, row.names = FALSE)
+# save registration data to s3 bucket
+cloudbrewr::aws_s3_store(
+  filename,
+  bucket = S3_BUCKET_NAME,
+  key = OUTPUT_KEY$household)
