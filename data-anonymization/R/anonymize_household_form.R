1a1e48452534f95463b30f90444aaac76c686e1b
Aryton Tediarjo <35942857+arytontediarjo@users.noreply.github.com>
2023-01-09 20:42:43 -0800

    update data anonymization with cloudbrewr

--- data-anonymization/R/anonymize_household_form.R
+++ data-anonymization/R/anonymize_household_form.R
@@ -1,7 +1,6 @@
-#' This script is used to replace hh_id with the encrypted format,
-#' and enable us to share the data as an open-source data repo
-#' @author atediarjo@gmail.com
-#' @reviewer joe@brew.cc
+# This script is used to replace hh_id with the encrypted format,
+# and enable us to share the data as an open-source data repo
+# @author atediarjo@gmail.com
 
 # import libraries
 library(dplyr)
@@ -9,20 +8,28 @@ library(data.table)
 library(digest)
 library(paws)
 library(glue)
-source("R/utils.R")
 
-# instantiate s3
-s3obj <- paws::s3()
+# set your pipeline stage here to define prod/dev environment
+# to test it locally do Sys.setenv(PIPELINE_STAGE = 'develop')
+cloudbrewr::aws_login(pipeline_stage = Sys.getenv('PIPELINE_STAGE'))
 
-# output list bucket and keys
-OUTPUT_LIST <- list(
-  bucket = glue::glue(Sys.getenv("BUCKET_PREFIX"), "databrew.org"),
-  anonymized_hh_s3_key = "kwale/public-form/public-reconbhousehold/public-reconbhousehold.csv",
-  hashmap_hh_s3_key = "kwale/hash-map/hash-map-reconbhousehold/hash-map-reconbhousehold.csv"
+# bucket name
+S3_BUCKET_NAME <- 'databrew.org'
+
+# input
+INPUT_KEY <- list(
+  household = 'kwale/clean-form/reconbhousehold/reconbhousehold.csv'
+)
+
+# output list keys
+OUTPUT_KEY <- list(
+  anonymized_household = "kwale/public-form/public-reconbhousehold/public-reconbhousehold.csv"
 )
 
 # get housedhold forms
-anonymized_hh <- get_household_forms() %>%
+anonymized_hh <- cloudbrewr::aws_s3_get_table(
+  bucket = S3_BUCKET_NAME,
+  key = INPUT_KEY$household) %>%
   dplyr::rowwise() %>%
   dplyr::mutate(hh_id = digest(hh_id, algo = "sha256"))
 
@@ -30,8 +37,8 @@ anonymized_hh <- get_household_forms() %>%
 # household pii mask save to s3 bucket
 filename <- tempfile(fileext = ".csv")
 anonymized_hh %>% fwrite(filename, row.names = FALSE)
-save_to_s3_bucket(
-  s3obj = s3obj,
-  file_path = filename,
-  bucket_name = OUTPUT_LIST$bucket,
-  object_key = OUTPUT_LIST$anonymized_hh_s3_key)
+# save registration data to s3 bucket
+cloudbrewr::aws_s3_store(
+  filename,
+  bucket = S3_BUCKET_NAME,
+  key = OUTPUT_KEY$anonymized_household)
