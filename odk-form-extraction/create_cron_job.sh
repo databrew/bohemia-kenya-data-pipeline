4602e33084d6597ac0cd9b793051b4ef293e733f
Aryton Tediarjo <35942857+arytontediarjo@users.noreply.github.com>
2023-04-09 17:21:38 -0700

    add cron job

---
+++ odk-form-extraction/create_cron_job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# create cron job
+sudo docker pull databrewllc/odk-form-extraction:production 
+# stop docker
+sudo stop odk_form_docker
+# remove docker
+sudo docker rm odk_form_docker
+# create detached interactive session
+sudo docker run -itd --env-file ~/databrew_kwale_key_vars --name odk_form_docker databrewllc/odk-form-extraction:production /bin/bash
+#write out current crontab
+crontab -l > odk_form_extraction_cron
+#echo new cron into cron file
+echo "* * * * * sudo docker exec -t odk_form_docker make pipeline >> ~/odk_run.log" > odk_form_extraction_cron
+#install new cron file
+crontab odk_form_extraction_cron
+# remove text
+rm odk_form_extraction_cron
