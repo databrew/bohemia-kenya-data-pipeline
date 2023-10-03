af4db29772ffceae482d6395e52f29ebb5441643
Aryton Tediarjo <35942857+arytontediarjo@users.noreply.github.com>
2023-10-03 19:24:41 +0700

    update new anomalies detector

---
+++ pipeline-cleaning/R/anomalies_detector/efficacy.R
@@ -0,0 +1,124 @@
+library(dplyr)
+library(lubridate)
+library(magrittr)
+library(paws)
+library(config)
+library(cloudbrewr)
+library(logger)
+library(config)
+library(tictoc)
+library(data.table)
+library(sf)
+library(sp)
+source('R/anomalies_utils.R')
+
+# start timer
+tic()
+
+# create log message
+logger::log_info('Starting Anomaly Detection for efficacy')
+
+# variables / creds
+env_pipeline_stage <- Sys.getenv("PIPELINE_STAGE")
+Sys.setenv(R_CONFIG_ACTIVE=env_pipeline_stage)
+BUCKET_NAME <- 'bohemia-lake-db'
+ROLE_NAME <- 'cloudbrewr-aws-role'
+OUTPUT_FILEPATH <- 'output/efficacy_anomalies_detection.csv'
+PROJECT_SOURCE <- 'kwale'
+
+unlink('report/data', recursive = TRUE)
+dir.create('report/data')
+
+# create connection to AWS
+tryCatch({
+  # login to AWS - this will be bypassed if executed in CI/CD environment
+  cloudbrewr::aws_login(
+    role_name = ROLE_NAME,
+    profile_name =  ROLE_NAME,
+    pipeline_stage = env_pipeline_stage)
+
+}, error = function(e){
+  logger::log_error('AWS Login Failed')
+  stop(e$message)
+})
+
+
+pad_hhid <- function(data){
+  if('hhid' %in% names(data)){
+    data %>%
+      dplyr::mutate(hhid = stringr::str_pad(hhid, 5, pad = "0"))
+  }else{
+    data
+  }
+}
+
+##############################
+## Fetch data
+##############################
+
+efficacy <- cloudbrewr::aws_s3_get_table(
+  bucket = 'databrew.org',
+  key = glue::glue('{PROJECT_SOURCE}/clean-form/efficacy/efficacy.csv')) %>%
+  pad_hhid()
+
+anomalies_list <- list()
+final_col_list <- c('KEY', 'form_id', 'anomalies_id', 'anomalies_description', 'anomalies_reports_to_wid')
+
+##############################
+## Anomalies Detecton
+##############################
+
+# 2 RDT tests invalid (alert the lab) in same visit-household
+anomalies_list$sus_rdt <- efficacy %>%
+  dplyr::filter(control_validity == 'invalid') %>%
+  dplyr::group_by(hhid) %>%
+  dplyr::mutate(n = n()) %>%
+  dplyr::filter(n >= 2) %>%
+  dplyr::mutate(form_id = 'efficacy',
+                anomalies_id = 'hh_2_rdt_invalid',
+                anomalies_description = glue::glue('hhid:{hhid} has {n} invalid test from control_validity'),
+                anomalies_reports_to_wid = glue::glue('{wid}')) %>%
+  dplyr::ungroup() %>%
+  dplyr::select(all_of(final_col_list))
+
+
+# Start and End time of RDT test
+anomalies_list$sus_rdt_time_diff <- efficacy %>%
+  dplyr::filter(rdt_time_diff < 10) %>%
+  dplyr::mutate(form_id = 'efficacy',
+                anomalies_id = 'hh_rdt_time_diff_less_than_10_mins',
+                anomalies_description = glue::glue('hhid:{hhid} rdt was collected in {rdt_time_diff}'),
+                anomalies_reports_to_wid = glue::glue('{wid}')) %>%
+  dplyr::ungroup() %>%
+  dplyr::select(all_of(final_col_list))
+
+# repeat visit number selected for individual
+anomalies_list$sus_individual_repeat_visit <- efficacy %>%
+  dplyr::group_by(hhid, extid, visit) %>%
+  dplyr::mutate(n = n()) %>%
+  dplyr::filter(n >= 1) %>%
+  dplyr::mutate(form_id = 'efficacy',
+                anomalies_id = glue::glue('ind_visit_already_in_dataset'),
+                anomalies_description = glue::glue('hhid:{hhid} extid:{extid}, visit:{visit} already in dataset'),
+                anomalies_reports_to_wid = glue::glue('{wid}')) %>%
+  dplyr::ungroup() %>%
+  dplyr::select(all_of(final_col_list))
+
+# hh gps accuracy too high
+anomalies_list$sus_gps <-  efficacy %>%
+  dplyr::filter(Accuracy > 15) %>%
+  dplyr::mutate(form_id = 'efficacy',
+                anomalies_id = 'hh_gps_accuracy_too_high',
+                anomalies_description = glue::glue('hhid:{hhid} gps accuracy too high: {Accuracy}'),
+                anomalies_reports_to_wid = glue::glue('{wid}')) %>%
+  dplyr::ungroup() %>%
+  dplyr::select(all_of(final_col_list))
+
+#################################
+## Consolidate
+#################################
+anomalies_list %>%
+  purrr::reduce(dplyr::bind_rows) %>%
+  fwrite(OUTPUT_FILEPATH)
+
+
---
+++ pipeline-cleaning/R/anomalies_detector/safetynew.R
@@ -0,0 +1,162 @@
+library(dplyr)
+library(lubridate)
+library(magrittr)
+library(paws)
+library(config)
+library(cloudbrewr)
+library(logger)
+library(config)
+library(tictoc)
+library(data.table)
+library(sf)
+library(sp)
+source('R/anomalies_utils.R')
+
+# start timer
+tic()
+
+# create log message
+logger::log_info('Starting Anomaly Detection for safetynew')
+
+# variables / creds
+env_pipeline_stage <- Sys.getenv("PIPELINE_STAGE")
+Sys.setenv(R_CONFIG_ACTIVE=env_pipeline_stage)
+BUCKET_NAME <- 'bohemia-lake-db'
+ROLE_NAME <- 'cloudbrewr-aws-role'
+OUTPUT_FILEPATH <- 'output/safetynew_anomalies_detection.csv'
+PROJECT_SOURCE <- 'kwale'
+
+unlink('report/data', recursive = TRUE)
+dir.create('report/data')
+
+# create connection to AWS
+tryCatch({
+  # login to AWS - this will be bypassed if executed in CI/CD environment
+  cloudbrewr::aws_login(
+    role_name = ROLE_NAME,
+    profile_name =  ROLE_NAME,
+    pipeline_stage = env_pipeline_stage)
+
+}, error = function(e){
+  logger::log_error('AWS Login Failed')
+  stop(e$message)
+})
+
+
+pad_hhid <- function(data){
+  if('hhid' %in% names(data)){
+    data %>%
+      dplyr::mutate(hhid = stringr::str_pad(hhid, 5, pad = "0"))
+  }else{
+    data
+  }
+}
+
+
+##############################
+## Fetch Safety New Data
+##############################
+safetynew <- cloudbrewr::aws_s3_get_table(
+  bucket = 'databrew.org',
+  key = glue::glue('{PROJECT_SOURCE}/clean-form/safetynew/safetynew.csv')) %>%
+  pad_hhid()
+
+safetynew_repeat_individual <- cloudbrewr::aws_s3_get_table(
+  bucket = 'databrew.org',
+  key = glue::glue('{PROJECT_SOURCE}/clean-form/safetynew/safetynew-repeat_individual.csv')
+)
+
+safetynew_merged_df <- safetynew_repeat_individual %>%
+  dplyr::inner_join(safetynew, by = c('PARENT_KEY'='KEY'))
+
+anomalies_list <- list()
+final_col_list <- c('KEY', 'form_id', 'anomalies_id', 'anomalies_description', 'anomalies_reports_to_wid')
+
+##############################
+## Anomalies Detecton
+##############################
+
+# visit number already in dataset
+anomalies_list$visit_already_in_dataset <- safetynew %>%
+  dplyr::group_by(hhid, visit) %>%
+  dplyr::mutate(n = n()) %>%
+  dplyr::filter(n > 1) %>%
+  dplyr::mutate(form_id = 'safetynew',
+                anomalies_id = glue::glue('hh_visit_already_in_dataset'),
+                anomalies_description = glue::glue('hhid:{hhid} visit:{visit} already in dataset'),
+                anomalies_reports_to_wid = glue::glue('{wid}')) %>%
+  dplyr::ungroup() %>%
+  dplyr::select(all_of(final_col_list))
+
+# suspicious height based on age
+anomalies_list$sus_height <- safety_merged_df %>%
+  dplyr::filter((age >= 12 & height < 100) | (age < 12 & height > 150)) %>%
+  dplyr::mutate(form_id = 'safetynew-repeat_individual',
+                anomalies_id = 'ind_suspicious_height_based_on_age',
+                anomalies_description = glue::glue('{extid} is {age} years old with height {height} cm'),
+                anomalies_reports_to_wid = glue::glue('{wid}')) %>%
+  dplyr::select(all_of(final_col_list))
+
+# suspicious weight based on age
+anomalies_list$sus_weight <- safety_merged_df %>%
+  dplyr::filter((age >= 12 & weight < 30) | (age < 12 & weight > 50)) %>%
+  dplyr::mutate(form_id = 'safetynew-repeat_individual',
+                anomalies_id = 'ind_suspicious_weight_based_on_age',
+                anomalies_description = glue::glue('Individual is {age} years old with weight {weight} kg'),
+                anomalies_reports_to_wid = glue::glue('{wid}')) %>%
+  dplyr::select(all_of(final_col_list))
+
+# abnormal BMIs
+anomalies_list$sus_bmi <- safety_merged_df %>%
+  dplyr::mutate(bmi = weight / ((height/100)**2)) %>%
+  dplyr::filter(bmi < 16 | bmi >35) %>%
+  dplyr::mutate(form_id = 'safetynew-repeat_individual',
+                anomalies_id = 'ind_suspicious_bmi',
+                anomalies_description = glue::glue('{extid} has bmi of {bmi}'),
+                anomalies_reports_to_wid = glue::glue('{wid}')) %>%
+  dplyr::select(all_of(final_col_list))
+
+# too many people leaving household
+anomalies_list$sus_died_or_migrated <- safety_merged_df %>%
+  dplyr::group_by(hhid) %>%
+  dplyr::mutate(died = n_distinct(extid[person_absent_reason == 'Died']),
+                migrated = n_distinct(extid[person_absent_reason == 'Migrated'])) %>%
+  dplyr::filter(died > 3 | migrated > 3) %>%
+  dplyr::mutate(form_id = 'safetynew-repeat_individual',
+                anomalies_id = 'hh_more_than_3_members_died_or_migrated',
+                anomalies_description = glue::glue('hhid:{hhid} members {died} died and {migrated} migrated'),
+                anomalies_reports_to_wid = glue::glue('{wid}')) %>%
+  dplyr::ungroup() %>%
+  dplyr::select(all_of(final_col_list))
+
+# too many absent members
+anomalies_list$sus_absent <- safety_merged_df %>%
+  dplyr::group_by(hhid) %>%
+  dplyr::mutate(absent = n_distinct(extid[person_absent_reason == 'Absent'])) %>%
+  dplyr::filter(absent > 3) %>%
+  dplyr::mutate(form_id = 'safetynew-repeat_individual',
+                anomalies_id = 'hh_more_than_3_members_absent',
+                anomalies_description = glue::glue('hhid:{hhid} members {absent} absent'),
+                anomalies_reports_to_wid = glue::glue('{wid}')) %>%
+  dplyr::ungroup() %>%
+  dplyr::select(all_of(final_col_list))
+
+# hh gps accuracy too high
+anomalies_list$sus_gps <-  safetynew %>%
+  dplyr::filter(Accuracy > 15) %>%
+  dplyr::mutate(form_id = 'safetynew',
+                anomalies_id = 'hh_gps_accuracy_too_high',
+                anomalies_description = glue::glue('hhid:{hhid} gps accuracy too high: {Accuracy}'),
+                anomalies_reports_to_wid = glue::glue('{wid}')) %>%
+  dplyr::ungroup() %>%
+  dplyr::select(all_of(final_col_list))
+
+#################################
+# Consolidate
+#################################
+anomalies_list %>%
+  purrr::reduce(dplyr::bind_rows) %>%
+  fwrite(OUTPUT_FILEPATH)
+
+
+
